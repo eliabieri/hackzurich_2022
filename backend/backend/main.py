acda4c19474387c3b6e9b9e5e805eb0e828652d2
Nando5011 <Nando.imboden@bluewin.ch>
2022-09-17 23:10:54 +0200

    Implemented basic acc/brk/left/right functionality for driving car

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,5 +1,37 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+import RPi.GPIO as GPIO
+
+
+CH1_PIN = 12
+CH2_PIN = 13
+PWM_FREQUENCY = 100000
+
+PWM_MAX_DC_PROMILLE = 1000
+PWM_75_DC_PROMILLE = 750
+PWM_NOM_DC_PROMILLE = 500
+PWM_MIN_DC_PROMILLE = 1
+
+MAX_SPEED = 500
+ACCELERATION_FACTOR = 1
+BRAKE_FACTOR = 1
+TURN_FACTOR = 5
+
+#Use BOARD numbering for pin numbers
+GPIO.setmode(GPIO.BCM)
+
+
+#Initialize GPIOs
+GPIO.setup(CH1_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(CH2_PIN, GPIO.OUT, initial=GPIO.LOW)
+
+
+#Initalize as PWM
+ch1Control = GPIO.PWM(CH1_PIN, PWM_FREQUENCY)
+ch2Control = GPIO.PWM(CH2_PIN, PWM_FREQUENCY)
+
+ch1SpeedPromille = 0
+ch2SpeedPromille = 0
 
 app = FastAPI()
 
@@ -15,5 +47,54 @@ app.add_middleware(
 
 @app.get("/drive/{direction}")
 def drive(direction: str):
+    #TODO switch statement, do actions based on button pressed
     print(direction)
+    #TODO exception handling
     return "Success"
+
+
+
+def updateSpeeds():
+    if ch1SpeedPromille > 0:
+        ch1Control.ChangeDutyCycle(ch1SpeedPromille/10)
+        print("CH1 running: " + str(ch1SpeedPromille/10) + "%")
+    else:
+        ch1Control.stop()
+        print("CH1 stopped.")
+    if ch2SpeedPromille > 0:
+        ch2Control.ChangeDutyCycle(ch2SpeedPromille/10)
+        print("CH2 running: " + str(ch2SpeedPromille/10) + "%")
+    else:
+        ch2Control.stop()
+        print("CH2 stopped.")
+
+def accelerate():
+    if (ch1SpeedPromille < MAX_SPEED) or (ch2SpeedPromille < MAX_SPEED):
+        if ch1SpeedPromille < MAX_SPEED:
+            #Start PWM if not already started
+            if ch1SpeedPromille == 0:
+                ch1Control.start(0.1)
+            ch1SpeedPromille = ch1SpeedPromille+ACCELERATION_FACTOR
+        if ch2SpeedPromille < MAX_SPEED:
+            #Start PWM if not already started
+            if ch2SpeedPromille == 0:
+                ch2Control.start(0.1)
+            ch2SpeedPromille = ch2SpeedPromille+ACCELERATION_FACTOR
+        updateSpeeds()
+
+def brake():
+    if ch1SpeedPromille > 0:
+        ch1SpeedPromille = ch1SpeedPromille-BRAKE_FACTOR
+    if ch2SpeedPromille > 0:
+        ch2SpeedPromille = ch2SpeedPromille-BRAKE_FACTOR
+    updateSpeeds()
+
+def turnLeft():
+    if ch1SpeedPromille < MAX_SPEED:
+        ch1SpeedPromille = ch1SpeedPromille+TURN_FACTOR
+        updateSpeeds()
+
+def turnRight():
+    if ch2SpeedPromille < MAX_SPEED:
+        ch2SpeedPromille = ch2SpeedPromille+TURN_FACTOR
+        updateSpeeds()
