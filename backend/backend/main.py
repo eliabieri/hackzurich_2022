d3ee107d5d41c0149af703f5bb40650b76770f66
Nando5011 <Nando.imboden@bluewin.ch>
2022-09-18 10:44:08 +0200

    Basic implementation of pulse signals

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,6 +1,7 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import RPi.GPIO as GPIO
+import time
 
 import logging
 from rich.logging import RichHandler
@@ -13,36 +14,19 @@ logging.basicConfig(
 
 log = logging.getLogger("rich")
 
-
-CH1_PIN = 12
-CH2_PIN = 13
-PWM_FREQUENCY = 100000
-
-PWM_MAX_DC_PROMILLE = 1000
-PWM_75_DC_PROMILLE = 750
-PWM_NOM_DC_PROMILLE = 500
-PWM_MIN_DC_PROMILLE = 1
-
-MAX_SPEED = 500
-ACCELERATION_FACTOR = 1
-BRAKE_FACTOR = 1
-TURN_FACTOR = 5
+UP_PIN = 5
+DOWN_PIN = 6
+LEFT_PIN = 12
+RIGHT_PIN = 13
 
 #Use BOARD numbering for pin numbers
 GPIO.setmode(GPIO.BCM)
 
-
 #Initialize GPIOs
-GPIO.setup(CH1_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(CH2_PIN, GPIO.OUT, initial=GPIO.LOW)
-
-
-#Initalize as PWM
-ch1Control = GPIO.PWM(CH1_PIN, PWM_FREQUENCY)
-ch2Control = GPIO.PWM(CH2_PIN, PWM_FREQUENCY)
-
-ch1SpeedPromille = 0
-ch2SpeedPromille = 0
+GPIO.setup(UP_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(DOWN_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LEFT_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIGHT_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 app = FastAPI()
 
@@ -58,78 +42,28 @@ app.add_middleware(
 
 @app.get("/drive/{direction}")
 def drive(direction: str):
-    global ch1SpeedPromille
-    global ch2SpeedPromille
     try:
+        #depending on direction, output a 10ms pulse on according output
         if direction == "up":
-            accelerate()
+            GPIO.output(UP_PIN, GPIO.HIGH)
+            time.sleep(0.01)
+            GPIO.output(UP_PIN, GPIO.LOW)
         elif direction == "down":
-            brake()
+            GPIO.output(DOWN_PIN, GPIO.HIGH)
+            time.sleep(0.01)
+            GPIO.output(DOWN_PIN, GPIO.LOW)
         elif direction == "left":
-            turnLeft()
+            GPIO.output(LEFT_PIN, GPIO.HIGH)
+            time.sleep(0.01)
+            GPIO.output(LEFT_PIN, GPIO.LOW)
         elif direction == "right":
-            turnRight()
+            GPIO.output(RIGHT_PIN, GPIO.HIGH)
+            time.sleep(0.01)
+            GPIO.output(RIGHT_PIN, GPIO.LOW)
         else:
             log.info("Invalid direction")
         log.info(direction)
-        return f"ch1Speed={ch1SpeedPromille/10} ch2Speed={ch2SpeedPromille/10}"
+        return "success"
     except Exception as e:
-        ch1Control.stop()
-        ch2Control.stop()
         return f"exception {e}"
 
-
-def updateSpeeds():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille > 0:
-        ch1Control.ChangeDutyCycle(ch1SpeedPromille/10)
-        log.info("CH1 running: " + str(ch1SpeedPromille/10) + "%")
-    else:
-        ch1Control.stop()
-        log.info("CH1 stopped.")
-    if ch2SpeedPromille > 0:
-        ch2Control.ChangeDutyCycle(ch2SpeedPromille/10)
-        log.info("CH2 running: " + str(ch2SpeedPromille/10) + "%")
-    else:
-        ch2Control.stop()
-        log.info("CH2 stopped.")
-
-def accelerate():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if (ch1SpeedPromille < MAX_SPEED) or (ch2SpeedPromille < MAX_SPEED):
-        if ch1SpeedPromille < MAX_SPEED:
-            #Start PWM if not already started
-            if ch1SpeedPromille == 0:
-                ch1Control.start(0.1)
-            ch1SpeedPromille = ch1SpeedPromille+ACCELERATION_FACTOR
-        if ch2SpeedPromille < MAX_SPEED:
-            #Start PWM if not already started
-            if ch2SpeedPromille == 0:
-                ch2Control.start(0.1)
-            ch2SpeedPromille = ch2SpeedPromille+ACCELERATION_FACTOR
-        updateSpeeds()
-
-def brake():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille > 0:
-        ch1SpeedPromille = ch1SpeedPromille-BRAKE_FACTOR
-    if ch2SpeedPromille > 0:
-        ch2SpeedPromille = ch2SpeedPromille-BRAKE_FACTOR
-    updateSpeeds()
-
-def turnLeft():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille < MAX_SPEED:
-        ch1SpeedPromille = ch1SpeedPromille+TURN_FACTOR
-        updateSpeeds()
-
-def turnRight():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch2SpeedPromille < MAX_SPEED:
-        ch2SpeedPromille = ch2SpeedPromille+TURN_FACTOR
-        updateSpeeds()
