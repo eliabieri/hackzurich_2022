0373813ecb412561acca84038f04fe4d2eac2782
Nando Imboden <bimbon@bbcag.ch>
2022-11-14 22:42:43 +0100

    Adapted backend to control servo with 50%...100% DC at 500Hz

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -14,35 +14,31 @@ logging.basicConfig(
 log = logging.getLogger("rich")
 
 
-CH1_PIN = 12
-CH2_PIN = 13
-PWM_FREQUENCY = 100000
-
-PWM_MAX_DC_PROMILLE = 1000
-PWM_75_DC_PROMILLE = 750
-PWM_NOM_DC_PROMILLE = 500
-PWM_MIN_DC_PROMILLE = 1
-
-MAX_SPEED = 500
-ACCELERATION_FACTOR = 1
-BRAKE_FACTOR = 1
+SERVO_PIN = 12
+PWM_FREQUENCY_SERVO_HZ = 500
+SERVO_FULL_LEFT_DC = 50
+SERVO_FULL_RIGHT_DC = 100
 TURN_FACTOR = 5
+
+MOTOR_IN1 = 13
+MOTOR_IN2 = 14
+
+
 
 #Use BOARD numbering for pin numbers
 GPIO.setmode(GPIO.BCM)
 
 
 #Initialize GPIOs
-GPIO.setup(CH1_PIN, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(CH2_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(SERVO_PIN, GPIO.OUT, initial=GPIO.LOW)
+# GPIO.setup(CH2_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 
 #Initalize as PWM
-ch1Control = GPIO.PWM(CH1_PIN, PWM_FREQUENCY)
-ch2Control = GPIO.PWM(CH2_PIN, PWM_FREQUENCY)
+servoControl = GPIO.PWM(SERVO_PIN, PWM_FREQUENCY_SERVO_HZ)
+# ch2Control = GPIO.PWM(CH2_PIN, PWM_FREQUENCY)
 
-ch1SpeedPromille = 0
-ch2SpeedPromille = 0
+servoDirection = 0
 
 app = FastAPI()
 
@@ -70,66 +66,80 @@ def drive(direction: str):
         elif direction == "right":
             turnRight()
         else:
+            goNeutral()
             log.info("Invalid direction")
         log.info(direction)
         return f"ch1Speed={ch1SpeedPromille/10} ch2Speed={ch2SpeedPromille/10}"
     except Exception as e:
-        ch1Control.stop()
-        ch2Control.stop()
+        servoControl.stop()
+        # ch2Control.stop()
         return f"exception {e}"
 
 
+#pass -100 for full left, pass +100 for full right
+def getServoPwm(direction):
+    if(direction < -100):
+        return 50
+    if(direction > 100):
+        return 100
+    #converts -100 (full left) to 50% duty cycle, and +100 to 100% duty cycle
+    return ((((direction+100)/100)*25) + 50)
+
 def updateSpeeds():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille > 0:
-        ch1Control.ChangeDutyCycle(ch1SpeedPromille/10)
-        log.info("CH1 running: " + str(ch1SpeedPromille/10) + "%")
+    global servoDirection
+    if servoDirection > -1 or servoDirection < 1:
+        servoControl.stop()
     else:
-        ch1Control.stop()
-        log.info("CH1 stopped.")
-    if ch2SpeedPromille > 0:
-        ch2Control.ChangeDutyCycle(ch2SpeedPromille/10)
-        log.info("CH2 running: " + str(ch2SpeedPromille/10) + "%")
-    else:
-        ch2Control.stop()
-        log.info("CH2 stopped.")
+        servoControl.ChangeDutyCycle(getServoPwm(servoDirection))
 
-def accelerate():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if (ch1SpeedPromille < MAX_SPEED) or (ch2SpeedPromille < MAX_SPEED):
-        if ch1SpeedPromille < MAX_SPEED:
-            #Start PWM if not already started
-            if ch1SpeedPromille == 0:
-                ch1Control.start(0.1)
-            ch1SpeedPromille = ch1SpeedPromille+ACCELERATION_FACTOR
-        if ch2SpeedPromille < MAX_SPEED:
-            #Start PWM if not already started
-            if ch2SpeedPromille == 0:
-                ch2Control.start(0.1)
-            ch2SpeedPromille = ch2SpeedPromille+ACCELERATION_FACTOR
-        updateSpeeds()
+# def accelerate():
+    # global ch1SpeedPromille
+    # global ch2SpeedPromille
+    # if (ch1SpeedPromille < MAX_SPEED) or (ch2SpeedPromille < MAX_SPEED):
+    #     if ch1SpeedPromille < MAX_SPEED:
+    #         #Start PWM if not already started
+    #         if ch1SpeedPromille == 0:
+    #             ch1Control.start(0.1)
+    #         ch1SpeedPromille = ch1SpeedPromille+ACCELERATION_FACTOR
+    #     if ch2SpeedPromille < MAX_SPEED:
+    #         #Start PWM if not already started
+    #         if ch2SpeedPromille == 0:
+    #             ch2Control.start(0.1)
+    #         ch2SpeedPromille = ch2SpeedPromille+ACCELERATION_FACTOR
+    #     updateSpeeds()
 
-def brake():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille > 0:
-        ch1SpeedPromille = ch1SpeedPromille-BRAKE_FACTOR
-    if ch2SpeedPromille > 0:
-        ch2SpeedPromille = ch2SpeedPromille-BRAKE_FACTOR
-    updateSpeeds()
+# def brake():
+    # global ch1SpeedPromille
+    # global ch2SpeedPromille
+    # if ch1SpeedPromille > 0:
+    #     ch1SpeedPromille = ch1SpeedPromille-BRAKE_FACTOR
+    # if ch2SpeedPromille > 0:
+    #     ch2SpeedPromille = ch2SpeedPromille-BRAKE_FACTOR
+    # updateSpeeds()
 
 def turnLeft():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch1SpeedPromille < MAX_SPEED:
-        ch1SpeedPromille = ch1SpeedPromille+TURN_FACTOR
+    global servoDirection
+    if servoDirection > (-100+TURN_FACTOR):
+        servoDirection = servoDirection - TURN_FACTOR
+    else:
+        servoDirection = -100
         updateSpeeds()
 
 def turnRight():
-    global ch1SpeedPromille
-    global ch2SpeedPromille
-    if ch2SpeedPromille < MAX_SPEED:
-        ch2SpeedPromille = ch2SpeedPromille+TURN_FACTOR
+    global servoDirection
+    if servoDirection < (100-TURN_FACTOR):
+        servoDirection = servoDirection + TURN_FACTOR
+    else:
+        servoDirection = 100
         updateSpeeds()
+
+def goNeutral():
+    global servoDirection
+    if(servoDirection < (2.5*TURN_FACTOR) or servoDirection > (-2.5*TURN_FACTOR)):
+        servoDirection = 0
+    else:
+        if(servoDirection > 0):
+            servoDirection = servoDirection - 2*TURN_FACTOR
+        elif(servoDirection < 0):
+            servoDirection = servoDirection + 2*TURN_FACTOR
+    updateSpeeds()
